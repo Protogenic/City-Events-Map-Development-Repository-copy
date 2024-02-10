440cbf8f9d719bf88ff6d369cc3c4d46c4d54b11
Владимир Безруков <82569672+Protogenic@users.noreply.github.com>
2024-02-10 12:41:49 +0300

    Add first version of address parser

--- newsReader.py
+++ newsReader.py
@@ -1,16 +1,42 @@
+import asyncio
+
 import requests
 from bs4 import BeautifulSoup
 import time
 # from models import News
 # from django.http import JsonResponse
-import psycopg2
+
+import g4f
+
+from g4f.Provider import (
+    Bard,
+    Bing,
+    HuggingChat,
+    OpenAssistant,
+    OpenaiChat,
+)
 
 # Сделаю ветку с django которая не работает друзья
 
-# conn = psycopg2.connect(host="localhost", dbname="nnru_reader", user="postgres", password="postgres")
-# cur = conn.cursor()
+news_limit = 1
 
-news_limit = 3
+promt = """На русском языке:
+       "1) Опишите заголовок события, уделяя внимание ключевым аспектам.
+        2)  Укажите место события в формате Город, улица (при наличии), дом (при наличии), и исключите лишние локационные детали.(Все события происходят В Нижнем Новгороде)
+        3) Переформулируйте информацию, предоставив более точный контекст."""
+
+
+async def parsing_func(input_text):
+    response = await g4f.ChatCompletion.create_async(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": promt + input_text}],
+        # stream=True,
+    )
+    output_text = ""
+    for msg in response:
+        output_text += msg
+    return output_text
+
 
 def get_after_find(article, par1, par2):
     tag = article.find(par1)
@@ -64,32 +90,7 @@ def check_response(url):
 #     response = requests.post(server, data=news)
 #     print(response.json())
 
-def rbc():
-    site_url = 'https://nn.rbc.ru/nn/'
-    data = check_response(site_url)
-    if data is None:
-        return
-
-
-    for article in data.find_all('div', {'class': 'item js-rm-central-column-item item_image-mob js-category-item'})[:news_limit]:
-        title = article.find('span', {'class': 'normal-wrap'}).text
-        date = article.find('span', {'class': 'item__category'}).text
-        url = article.find('a', {'class': 'item__link rm-cm-item-link js-rm-central-column-item-link'}).get('href')
-        img = get_after_find(article, 'img', 'src')
-
-        response = requests.get(url)
-        news_data = BeautifulSoup(response.text, 'html.parser')
-        text = ''
-        if news_data.find('p') is not None:
-            text = news_data.find('p').text
-
-        print(title, '\n', text, '\n', date, '\n', url, '\n', img, '\n\n\n')
-        #pass_news(0, title, '', date, '', url, img)
-
-
-
-
-def nnru():
+async def nnru():
     site_url = 'https://www.nn.ru/text/'
     data = check_response(site_url)
     if data is None:
@@ -111,18 +112,42 @@ def nnru():
         if news_data.find('div', {'class': 'qQq9J'}) is not None:
             text = news_data.find('div', {'class': 'qQq9J'}).text
 
+        post = await parsing_func(text)
+        print(post)
+
+        print(title, '\n', text, '\n', date, '\n', url, '\n', img, '\n\n\n')
+        #pass_news(0, title, '', date, '', url, img)
+
+async def rbc():
+    site_url = 'https://nn.rbc.ru/nn/'
+    data = check_response(site_url)
+    if data is None:
+        return
+
+
+    for article in data.find_all('div', {'class': 'item js-rm-central-column-item item_image-mob js-category-item'})[:news_limit]:
+        title = article.find('span', {'class': 'normal-wrap'}).text
+        date = article.find('span', {'class': 'item__category'}).text
+        url = article.find('a', {'class': 'item__link rm-cm-item-link js-rm-central-column-item-link'}).get('href')
+        img = get_after_find(article, 'img', 'src')
+
+        response = requests.get(url)
+        news_data = BeautifulSoup(response.text, 'html.parser')
+        text = ''
+        if news_data.find('p') is not None:
+            text = news_data.find('p').text
+
+        post = await parsing_func(text)
+        print(post)
+
         print(title, '\n', text, '\n', date, '\n', url, '\n', img, '\n\n\n')
         #pass_news(0, title, '', date, '', url, img)
 
 
 
-
 while True:
     print("NN.RU:\n")
-    nnru()
+    asyncio.run(nnru())
     print("RBC:\n")
-    rbc()
+    asyncio.run(rbc())
     time.sleep(60)
-
-# cur.close()
-# conn.close()
