11c9ae8402c07fab352e5b4916c7ad29ede17163
Valeriy <108849165+OneDpsss@users.noreply.github.com>
2024-02-10 12:16:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 27321962
-api_hash = "79f2103e308cdf9d5ad428f4f7683b52"
+api_id = ""
+api_hash = ""
 
-bot_token = "6773525729:AAGc2R-8epyXEgl3kFJp20bSsOImd_w-rsM"
+bot_token = ""
 
 my_chat_id = -1002146311106
