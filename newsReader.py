fa6f3bd73f45b5a54742bb24b156047c260ade1f
Владимир Безруков <82569672+Protogenic@users.noreply.github.com>
2024-02-10 14:26:56 +0300

    Almost fix address parser

--- newsReader.py
+++ newsReader.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -20,16 +19,16 @@ from g4f.Provider import (
 
 news_limit = 1
 
-promt = """На русском языке:
-       "1) Опишите заголовок события, уделяя внимание ключевым аспектам.
-        2)  Укажите место события в формате Город, улица (при наличии), дом (при наличии), и исключите лишние локационные детали.(Все события происходят В Нижнем Новгороде)
-        3) Переформулируйте информацию, предоставив более точный контекст."""
-
+prompt = """На русском языке:
+    1) Опишите заголовок события, обращая внимание на ключевые аспекты и исключая дополнительные детали.
+    2) Укажите только место события: только существующий город (при наличии), улица (при наличии), дом (при наличии), и исключите излишние локационные детали, не включать в адрес тип местности (поле, лес). (Все события происходят в Нижегородской области, по умолчанию адрес Нижний Новгород). Не надо в начале писать 2) Место события, только сам адрес
+    3) Переформулируйте информацию, предоставив более точный контекст и избегая лишних эмоциональных вставок.
+    """
 
 async def parsing_func(input_text):
     response = await g4f.ChatCompletion.create_async(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": promt + input_text}],
+        messages=[{"role": "user", "content": prompt + input_text}],
         # stream=True,
     )
     output_text = ""
@@ -55,7 +54,6 @@ def check_response(url):
         print('Failed to send request. Status code:', response.status_code)
         return None
 
-
 # вот тут вообще не понял как это должно работать друзья
 # def add_to_database(request):
 #     if request.method == 'POST':
@@ -74,7 +72,6 @@ def check_response(url):
 #     else:
 #         return JsonResponse({'error': 'Error'}, status=405)
 
-
 # def pass_news(id, title, description, date, place, url, img):
 #     server = 'http://kerilserver.com/add_data/'  # адрес сервера Керила
 #     news = {
@@ -89,6 +86,7 @@ def check_response(url):
 #
 #     response = requests.post(server, data=news)
 #     print(response.json())
+
 
 async def nnru():
     site_url = 'https://www.nn.ru/text/'
@@ -108,14 +106,15 @@ async def nnru():
 
         response = requests.get(url)
         news_data = BeautifulSoup(response.text, 'html.parser')
-        text = ''
+        text = title
         if news_data.find('div', {'class': 'qQq9J'}) is not None:
-            text = news_data.find('div', {'class': 'qQq9J'}).text
+            text += ". " + news_data.find('div', {'class': 'qQq9J'}).get_text(strip=True)
 
         post = await parsing_func(text)
-        print(post)
+        lines = post.split('\n')
+        place = lines[1][18:]
 
-        print(title, '\n', text, '\n', date, '\n', url, '\n', img, '\n\n\n')
+        print(title, '\n', text, '\n', date, '\n', place, '\n', url, '\n', img, '\n\n\n')
         #pass_news(0, title, '', date, '', url, img)
 
 async def rbc():
@@ -123,7 +122,6 @@ async def rbc():
     data = check_response(site_url)
     if data is None:
         return
-
 
     for article in data.find_all('div', {'class': 'item js-rm-central-column-item item_image-mob js-category-item'})[:news_limit]:
         title = article.find('span', {'class': 'normal-wrap'}).text
@@ -134,20 +132,19 @@ async def rbc():
         response = requests.get(url)
         news_data = BeautifulSoup(response.text, 'html.parser')
         text = ''
-        if news_data.find('p') is not None:
-            text = news_data.find('p').text
+        if news_data.find('div', {'class': 'article__text__overview'}) is not None:
+            text = news_data.find('div', {'class': 'article__text__overview'}).get_text(strip=True)
 
         post = await parsing_func(text)
-        print(post)
+        lines = post.split('\n')
+        place = lines[1][18:]
 
-        print(title, '\n', text, '\n', date, '\n', url, '\n', img, '\n\n\n')
-        #pass_news(0, title, '', date, '', url, img)
-
-
+        print(title, '\n', text, '\n', date, '\n', place, '\n', url, '\n', img, '\n\n\n')
+        # pass_news(0, title, '', date, '', url, img)
 
 while True:
     print("NN.RU:\n")
     asyncio.run(nnru())
     print("RBC:\n")
     asyncio.run(rbc())
-    time.sleep(60)
+    time.sleep(300)
