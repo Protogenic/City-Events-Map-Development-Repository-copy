c96f235052eeae8a0f5ecb274f240551a8484a16
Valeriy <108849165+OneDpsss@users.noreply.github.com>
2024-02-10 13:47:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ from g4f.Provider import (
 
 # Usage:
 
-promt = """На русском языке:
-       "1) Опишите заголовок события, уделяя внимание ключевым аспектам.
-        2)  Укажите место события в формате Город, улица (при наличии), дом (при наличии), и исключите лишние локационные детали.(Все события происходят В Нижнем Новгороде)
-        3) Переформулируйте информацию, предоставив более точный контекст."""
-
+prompt = """На русском языке:
+    1) Опишите заголовок события, обращая внимание на ключевые аспекты и исключая дополнительные детали.
+    2) Укажите место события в формате: Город (при наличии), улица (при наличии), дом (при наличии). Уточните, что все события происходят в Нижегородской области.
+    3) Переформулируйте информацию, предоставив более точный контекст и избегая лишних эмоциональных вставок.
+    """
 
 async def parsing_func(input_text):
     response = await g4f.ChatCompletion.create_async(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": promt + input_text}],
+        messages=[{"role": "user", "content": input_text + prompt}],
         # stream=True,
     )
     output_text = ""
@@ -39,32 +39,32 @@ asyncio.set_event_loop(loop)
 bot = TelegramClient('bot', api_id, api_hash, loop=loop)
 bot.start(bot_token=bot_token)
 
+
+async def send_message_func(text):
+    await bot.send_message(entity=my_chat_id, parse_mode="html", link_preview=False, message=text)
+
+
 telegram_channels = [-1001941183965]
 
 
 def check_ban_words(text):
     words = text.lower().split()
-
     key_words = [
+        "реклам",
+        "розыгрыш",
 
     ]
 
     for word in words:
-        if 'газ' in word and len(word) < 6:  # газ, газу, газом, газа
-            return True
-
         for key in key_words:
             if key in word:
-                return True
+                return False
 
-    return False
-
-
-async def send_message_func(text):
-    await bot.send_message(entity=my_chat_id, message=text)
+    return True
 
 
-client = telegram_parser(session, api_id, api_hash, telegram_channels, parse_func=parsing_func, loop=loop)
+client = telegram_parser(session, api_id, api_hash, telegram_channels, check_ban_words=check_ban_words,
+                         parse_func=parsing_func, loop=loop, send_message_func=send_message_func)
 
 try:
     client.run_until_disconnected()
--- tg_parser.py
+++ tg_parser.py
@@ -6,13 +6,14 @@ def telegram_parser(session, api_id, api_hash, telegram_channels, check_ban_word
                     send_message_func=None, loop=None):
     client = TelegramClient(session, api_id, api_hash, system_version="4.16.30-vxCUSTOM", loop=loop)
     client.start()
-
     @client.on(events.NewMessage(chats=telegram_channels))
     async def my_event_handler(event):
-        print(type(event.raw_text))
         one_line_text = event.raw_text.replace("\n", " ")
-        print(one_line_text)
-        post = await parse_func(one_line_text)
-        await client.send_message(my_chat_id, post)
+        if not (check_ban_words is None):
+            if not (check_ban_words(one_line_text)):
+                print("Не прошло")
+                return
 
+        post = await parse_func(one_line_text)
+        await send_message_func(post)
     return client
