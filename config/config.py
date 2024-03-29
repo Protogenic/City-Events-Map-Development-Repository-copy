5cd01f28acc4a4e95f298f2e486c7a44437ef647
Valeriy <108849165+OneDpsss@users.noreply.github.com>
2024-03-29 15:12:05 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
-api_id = 27321962
-api_hash = "79f2103e308cdf9d5ad428f4f7683b52"
-SHUTTLE_KEY = "shuttle-40e38ru11fwb0gs0rdh1"
-bot_token = "6773525729:AAGc2R-8epyXEgl3kFJp20bSsOImd_w-rsM"
-my_chat_id = -1002146311106
+api_id = 
+api_hash = ""
+SHUTTLE_KEY = ""
+bot_token = ""
+my_chat_id = 
 
 prompt = """In Russian in json format:
    {
