010c537aceae90e55dec301bcb2d114ef7c0f529
Владимир Безруков <82569672+Protogenic@users.noreply.github.com>
2024-04-05 10:49:22 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,11 +11,5 @@ Street (if applicable), Building (if applicable). Note that all events take plac
 Without splitting into separate entities.", "description": "Rephrase the information, providing a more precise 
 context and avoiding unnecessary emotional inserts."}"""
 
-prompt = """In Russian in json format:
-   {
-    "title": "Describe the event title, focusing on key aspects and excluding additional details.",
-    "location": "Specify the event location in the format: City (if applicable), Street (if applicable), Building (if applicable). Note that all events take place in the Nizhny Novgorod region. Without splitting into separate entities.",
-    "description": "Rephrase the information, providing a more precise context and avoiding unnecessary emotional inserts."
-    }
-    """
+
 
